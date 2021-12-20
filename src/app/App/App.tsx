01e4b1b32ac772e231a2a585e4944e3087a473b8
leonardo <leolub150@gmail.com>
2021-12-20 13:27:50 -0300

    refactor: move app to app folder

--- src/app/App/App.tsx
+++ src/app/App/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from '../../styles/global';
+import { Routes } from '../../routes';
 
-import { Routes } from './routes';
-
-import { GlobalStyle } from './styles/global';
-
-const App: React.FC = () => (
+export const App: React.FC = () => (
   <>
     <GlobalStyle />
     <BrowserRouter>
@@ -13,5 +11,3 @@ const App: React.FC = () => (
     </BrowserRouter>
   </>
 );
-
-export default App;
