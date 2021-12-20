61183a5f497112534ee3efe5fdb61b67bab48b42
leonardo <leolub150@gmail.com>
2021-12-20 18:23:31 -0300

    feat: add http statusCodes and method

---
+++ src/http/codes/httpMethods.ts
@@ -0,0 +1,6 @@
+export enum HttpMethods {
+  get = 'GET',
+  post = 'POST',
+  delete = 'DELETE',
+  put = 'PUT',
+}
---
+++ src/http/codes/httpStatusCodes.ts
@@ -0,0 +1,5 @@
+export enum HttpStatusCodes {
+  ok = 200,
+  notFound = 404,
+  serverError = 500,
+}
