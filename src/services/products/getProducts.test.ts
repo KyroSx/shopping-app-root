6c38bfea4d7414a7f917497c4cf5a728896146b1
leonardo <leolub150@gmail.com>
2021-12-21 13:40:34 -0300

    feat(services): ensure getProducts returns product list if request succeeds

--- src/services/products/getProducts.test.ts
+++ src/services/products/getProducts.test.ts
@@ -1,11 +1,28 @@
-import { getProducts } from '@/services/products';
+import { getProducts, Products } from '@/services/products';
 import { request } from '@/http/request';
 import { HttpMethods } from '@/http/codes';
 import { Endpoints } from '@/services/endpoints';
+import { makeHttpResponse } from '@/utils/testing';
 
 jest.mock('@/http/request/request');
 
 describe(getProducts, () => {
+  const mockRequestToSucceed = (products: Products) =>
+    (request as jest.Mock).mockReturnValue(
+      makeHttpResponse({
+        body: products,
+      }),
+    );
+
+  function makeProducts() {
+    return [
+      { id: 1, name: 'Banana', price: 10.0, available: 10 },
+      { id: 2, name: 'Apple', price: 20.0, available: 15 },
+      { id: 3, name: 'Orange', price: 30.0, available: 8 },
+      { id: 4, name: 'Mango', price: 15.0, available: 20 },
+    ];
+  }
+
   it('calls request with correct params', async () => {
     await getProducts();
 
@@ -13,5 +30,14 @@ describe(getProducts, () => {
       url: Endpoints.products.all(),
       method: HttpMethods.get,
     });
+  });
+
+  it('returns product list if request succeeds', async () => {
+    const products = makeProducts();
+    mockRequestToSucceed(products);
+
+    const productsOutput = await getProducts();
+
+    expect(productsOutput).toEqual(products);
   });
 });
--- src/services/products/getProducts.ts
+++ src/services/products/getProducts.ts
@@ -2,9 +2,19 @@ import { request } from '@/http/request';
 import { HttpMethods } from '@/http/codes';
 import { Endpoints } from '@/services/endpoints';
 
-export async function getProducts() {
-  await request({
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+  available: number;
+};
+export type Products = Product[];
+
+export async function getProducts(): Promise<Products> {
+  const response = await request<Products>({
     url: Endpoints.products.all(),
     method: HttpMethods.get,
   });
+
+  return response?.body;
 }
