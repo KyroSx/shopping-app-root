4b38c95f45eb57229de3b9ed2a375b9abe6797ad
leonardo <leolub150@gmail.com>
2021-12-22 15:28:49 -0300

    feat(ui): add theme provider

---
+++ src/app/providers/Theme/theme.ts
@@ -0,0 +1,36 @@
+export const theme = {
+  colors: {
+    slate: {
+      x500: '#64748B',
+      x600: '#475569',
+      x700: '#334155',
+      x800: '#1E293B',
+      x900: '#0F172A',
+    },
+
+    gray: {
+      x50: '#F9FAFB',
+      x500: '#6B7280',
+      x600: '#4B5563',
+      x700: '#374151',
+      x800: '#1F2937',
+      x900: '#111827',
+    },
+  },
+
+  space: {
+    none: '0',
+    smallest: '6px',
+    smaller: '8px',
+    small: '12px',
+    regular: '16px',
+    medium: '20px',
+    large: '24px',
+    larger: '32px',
+    largest: '40px',
+    loose: '56px',
+    looser: '64px',
+    loosest: '80px',
+    huge: '88px',
+  },
+};
