53ac8bf06b19c287951584cbb180ebe5354932d6
leonardo <leolub150@gmail.com>
2021-12-25 09:47:53 -0300

    feat(config): ensure env returns isDevelopment based on NODE_ENV

--- src/config/environment/environment.test.ts
+++ src/config/environment/environment.test.ts
@@ -1,8 +1,31 @@
 import { Env } from '@/config/environment';
 
 describe('Env', () => {
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...OLD_ENV };
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV;
+  });
+
   it('returns main-api-url', () => {
     process.env.REACT_APP_BASE_API_URL = 'http://main-api-url.com';
     expect(Env.apiUrl()).toBe('http://main-api-url.com');
+  });
+
+  describe('isDevelopment', () => {
+    it('returns true if NODE_ENV is "development"', () => {
+      // @ts-ignore
+      process.env.NODE_ENV = 'development';
+      expect(Env.isDevelopment()).toBe(true);
+    });
+
+    it('returns true if NODE_ENV is "development"', () => {
+      expect(Env.isDevelopment()).toBe(false);
+    });
   });
 });
--- src/config/environment/environment.ts
+++ src/config/environment/environment.ts
@@ -2,6 +2,10 @@ class Environment {
   apiUrl() {
     return process.env.REACT_APP_BASE_API_URL;
   }
+
+  isDevelopment() {
+    return process.env.NODE_ENV === 'development';
+  }
 }
 
 export const Env = new Environment();
