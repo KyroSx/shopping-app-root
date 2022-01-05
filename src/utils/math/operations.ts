ef597b48dc057e71215dd7eac06c6934e1d1de54
leonardo.gabriel <leolub150@gmail.com>
2022-01-05 13:15:27 -0300

    fix: ensure total also sum shipping

--- src/utils/math/operations.ts
+++ src/utils/math/operations.ts
@@ -1,2 +1,5 @@
 export const increment = (number: number) => number + 1;
 export const decrement = (number: number) => number - 1;
+
+export const sum = (...numbers: number[]) =>
+  numbers.reduce((total, number) => total + number, 0);
