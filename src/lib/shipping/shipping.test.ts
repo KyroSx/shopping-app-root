debcbabccd8c5b75e784bf076da78699cc7e64bc
leonardo.gabriel <leolub150@gmail.com>
2022-01-05 10:52:14 -0300

    feat: ensure shipping is MIN_WEIGHT_SHIPPING_VALUE if total weight is below or equal LIMIT_FOR_MIN_WEIGHT_SHIPPING

--- src/lib/shipping/shipping.test.ts
+++ src/lib/shipping/shipping.test.ts
@@ -2,6 +2,8 @@ import {
   calculateShipping,
   FREE_SHIPPING_VALUE,
   LIMIT_FOR_FREE_SHIPPING,
+  LIMIT_FOR_MIN_WEIGHT_SHIPPING,
+  MIN_WEIGHT_SHIPPING_VALUE,
 } from '@/lib/shipping/shipping';
 import { makeProductInCart } from '@/utils/testing/factories/products';
 import { ProductsInCart } from '@/types';
@@ -24,5 +26,15 @@ describe(calculateShipping, () => {
       makeProductsInCartForShipping({ quantity: 10, price: 41 }),
     );
     expect(shipping).toBe(FREE_SHIPPING_VALUE);
+  });
+
+  it(`is ${MIN_WEIGHT_SHIPPING_VALUE} if total weight is below or equal ${LIMIT_FOR_MIN_WEIGHT_SHIPPING}`, () => {
+    const shipping = calculateShipping(
+      makeProductsInCartForShipping({
+        quantity: LIMIT_FOR_MIN_WEIGHT_SHIPPING,
+        price: 1,
+      }),
+    );
+    expect(shipping).toBe(MIN_WEIGHT_SHIPPING_VALUE);
   });
 });
--- src/lib/shipping/shipping.ts
+++ src/lib/shipping/shipping.ts
@@ -6,7 +6,7 @@ export const LIMIT_FOR_FREE_SHIPPING = 400;
 export const FREE_SHIPPING_VALUE = 0;
 
 export const LIMIT_FOR_MIN_WEIGHT_SHIPPING = 10;
-export const MIN_WEIGHT_SHIPPING_PRICE = 30;
+export const MIN_WEIGHT_SHIPPING_VALUE = 30;
 
 export const LIMIT_OF_WEIGHT = 5;
 export const PRICE_PER_RATE_OF_WEIGHT = 7;
@@ -15,8 +15,15 @@ export function calculateShipping(products: ProductsInCart) {
   const weight = getTotalWeight(products);
   const subtotal = calculateSubtotal(products);
 
+  if (isAboveLimit(subtotal)) return FREE_SHIPPING_VALUE;
+  if (isBelowLimit(weight)) return MIN_WEIGHT_SHIPPING_VALUE;
+
   return FREE_SHIPPING_VALUE;
 }
 
 const getTotalWeight = (products: ProductsInCart) =>
   products.reduce((total, item) => total + item.quantity, 0);
+
+const isAboveLimit = (subtotal: number) => subtotal > LIMIT_FOR_FREE_SHIPPING;
+const isBelowLimit = (weight: number) =>
+  weight <= LIMIT_FOR_MIN_WEIGHT_SHIPPING;
