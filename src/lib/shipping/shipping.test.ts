88746acb851f2f529a880105042072654f2d952b
leonardo.gabriel <leolub150@gmail.com>
2022-01-05 10:58:21 -0300

    feat: ensure shipping is +VALUE_PER_RATE_OF_WEIGHT for each LIMIT_OF_WEIGHT kg above LIMIT_FOR_MIN_WEIGHT_SHIPPING kg

--- src/lib/shipping/shipping.test.ts
+++ src/lib/shipping/shipping.test.ts
@@ -3,7 +3,9 @@ import {
   FREE_SHIPPING_VALUE,
   LIMIT_FOR_FREE_SHIPPING,
   LIMIT_FOR_MIN_WEIGHT_SHIPPING,
+  LIMIT_OF_WEIGHT,
   MIN_WEIGHT_SHIPPING_VALUE,
+  VALUE_PER_RATE_OF_WEIGHT,
 } from '@/lib/shipping/shipping';
 import { makeProductInCart } from '@/utils/testing/factories/products';
 import { ProductsInCart } from '@/types';
@@ -36,5 +38,39 @@ describe(calculateShipping, () => {
       }),
     );
     expect(shipping).toBe(MIN_WEIGHT_SHIPPING_VALUE);
+  });
+
+  it(`is +${VALUE_PER_RATE_OF_WEIGHT} for each ${LIMIT_OF_WEIGHT}kg above ${LIMIT_FOR_MIN_WEIGHT_SHIPPING}kg`, () => {
+    let shipping = calculateShipping(
+      makeProductsInCartForShipping({
+        quantity: 14,
+        price: 1,
+      }),
+    );
+    expect(shipping).toBe(30);
+
+    shipping = calculateShipping(
+      makeProductsInCartForShipping({
+        quantity: 15,
+        price: 1,
+      }),
+    );
+    expect(shipping).toBe(37);
+
+    shipping = calculateShipping(
+      makeProductsInCartForShipping({
+        quantity: 16,
+        price: 1,
+      }),
+    );
+    expect(shipping).toBe(37);
+
+    shipping = calculateShipping(
+      makeProductsInCartForShipping({
+        quantity: 25,
+        price: 1,
+      }),
+    );
+    expect(shipping).toBe(51);
   });
 });
--- src/lib/shipping/shipping.ts
+++ src/lib/shipping/shipping.ts
@@ -2,6 +2,9 @@
 import { ProductsInCart } from '@/types';
 import { calculateSubtotal } from '@/lib/financial';
 
+type Math = (n: number) => number;
+type Bool = (n: number) => boolean;
+
 export const LIMIT_FOR_FREE_SHIPPING = 400;
 export const FREE_SHIPPING_VALUE = 0;
 
@@ -9,21 +12,36 @@ export const LIMIT_FOR_MIN_WEIGHT_SHIPPING = 10;
 export const MIN_WEIGHT_SHIPPING_VALUE = 30;
 
 export const LIMIT_OF_WEIGHT = 5;
-export const PRICE_PER_RATE_OF_WEIGHT = 7;
+export const VALUE_PER_RATE_OF_WEIGHT = 7;
 
 export function calculateShipping(products: ProductsInCart) {
-  const weight = getTotalWeight(products);
+  const weight = calculateWeight(products);
   const subtotal = calculateSubtotal(products);
 
   if (isAboveLimit(subtotal)) return FREE_SHIPPING_VALUE;
   if (isBelowLimit(weight)) return MIN_WEIGHT_SHIPPING_VALUE;
 
-  return FREE_SHIPPING_VALUE;
+  return calculateOverWeightLimit(weight);
 }
 
-const getTotalWeight = (products: ProductsInCart) =>
+const calculateWeight = (products: ProductsInCart) =>
   products.reduce((total, item) => total + item.quantity, 0);
 
-const isAboveLimit = (subtotal: number) => subtotal > LIMIT_FOR_FREE_SHIPPING;
-const isBelowLimit = (weight: number) =>
-  weight <= LIMIT_FOR_MIN_WEIGHT_SHIPPING;
+const isAboveLimit: Bool = subtotal => subtotal > LIMIT_FOR_FREE_SHIPPING;
+const isBelowLimit: Bool = weight => weight <= LIMIT_FOR_MIN_WEIGHT_SHIPPING;
+
+const calculateOverWeightLimit: Math = weight => {
+  if (isDivisibleByFive(weight)) return fx(weight);
+
+  return fx(predDivisibleByFive(weight));
+};
+
+const isDivisibleByFive: Bool = num => num % 5 === 0;
+const predDivisibleByFive: Math = num => num - (num % 5);
+const fx: Math = x => {
+  return (
+    ((x - LIMIT_FOR_MIN_WEIGHT_SHIPPING) / LIMIT_OF_WEIGHT) *
+      VALUE_PER_RATE_OF_WEIGHT +
+    MIN_WEIGHT_SHIPPING_VALUE
+  );
+};
