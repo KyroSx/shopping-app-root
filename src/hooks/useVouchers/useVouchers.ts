c0bfad57fff373d10066df420bfb02d5409a9ba0
kyroSx <leolub150@gmail.com>
2022-11-27 14:44:14 -0300

    refactor: ensure hooks use useQueryHandler instead of useQuery

--- src/hooks/useVouchers/useVouchers.ts
+++ src/hooks/useVouchers/useVouchers.ts
@@ -1,10 +1,14 @@
-import { useQuery } from 'react-query';
 import { getVouchers } from '@/services/vouchers';
 import { Voucher } from '@/types';
 import { getVoucherOrNull } from '@/lib/voucher';
+import { useQueryHandler } from '@/infra';
 
 export function useVouchers() {
-  const query = useQuery('vouchers', getVouchers, { enabled: false });
+  const query = useQueryHandler({
+    key: ['vouchers'],
+    function: getVouchers,
+    enabled: false,
+  });
 
   const getVoucherByCode = async (code?: string): Promise<Voucher | null> => {
     const vouchers = await query.refetch();
